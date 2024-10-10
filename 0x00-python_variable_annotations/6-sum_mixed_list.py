acc81645466dd535cf32b46c9258b3ebd943c407
Lagzal <mlagzal.sayhi@gmail.com>
2024-10-10 09:17:04 +0000

    6.7. We nned to explecitly use typing Union

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """ This a task module """
 from functools import reduce
-from typing import List
+from typing import List, Union
 
 
-def sum_mixed_list(mxd_list: List[float | int]) -> float:
+def sum_mixed_list(mxd_list: List[Union[int, float]]) -> float:
     """Reduce list"""
     return reduce(lambda e, p: e + p, mxd_list)
--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """ This a task module """
 from functools import reduce
-from typing import Tuple
+from typing import Tuple, Union
 
 
-def to_kv(k: str, v: int | float) -> Tuple[str, int | float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, int | float]:
     """tuple annotation"""
     return (k, v)
